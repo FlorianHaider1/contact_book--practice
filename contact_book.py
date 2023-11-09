63d4283b2d423b230ec0ae5b643e5c84dbcfe3b2
Florian Haider <149242442+FlorianHaider1@users.noreply.github.com>
2023-11-09 13:51:26 +0100

    Explain task and goals

---
+++ contact_book.py
@@ -0,0 +1,7 @@
+# Create a program to manage a contact book.
+# Each contact can have a name, phone number, email, and address.
+# Practice Skills:
+# Use a dictionary to store contacts (with names as keys).
+# Use lists and tuples to store contact details.
+# Practice adding, removing, and editing contacts, and searching for a specific contact.
+# Implement string methods for formatting and validating input (like email format validation).
